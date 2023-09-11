8d06260b7788178e53732c7e4f16ce7e91712b12
Johanneth <jaagav4@gmail.com>
2023-09-11 14:09:32 -0300

    creando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,57 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Home from "./pages/Home"
+import Single from "./pages/Single" 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Hoome!</div>,
+  },
+
+  {
+    path: "/register",
+    element: <Register/>,
+    
+  },
+  
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+
+
+]);
+
+
+
+
 function App() {
-  return <div> Bienvenido! </div>;
+  
+  return (
+    <div>
+      <RouterProvider router={router}/>      
+    </div>  
+  ) 
 }
 
 export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Resgister = () => {
+const Register = () => {
     return (
         <div>Register</div>
     )
