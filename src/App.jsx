7bb472cf147f812be2757ee9cc5f110c21523c6d
Johanneth <jaagav4@gmail.com>
2023-09-11 21:44:53 -0300

    Se crean los estilos de Login y Register y se comienza a diseñar el Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Home from "./pages/Home"
 import Single from "./pages/Single" 
 import Navbar from "./componets/Navbar";
 import Footer from "./componets/Footer";
-import "./style.css"
+import "./style.scss"
 
 
 const Layout = () =>{
--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from "../img/logo.png"
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt="" />                        
+                </div>
+                <div className="links">
+                    <Link className='link' to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <span>Joha</span>
+                    <span>Cerrar Sesión</span>
+                    <span className='write'>
+                        <Link to="/write">Escribir</Link>
+                    </span>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
 export default Navbar
+
