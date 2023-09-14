1bccdf900025e38124dadce42b7c6c8c29691bad
Johanneth <jaagav4@gmail.com>
2023-09-14 17:14:05 -0300

    hice varias modificaciones porque el vsc me dejo de andar tuve q formatear la pc y los archivos me tiran error, ya se hace la conexion con mysql

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import { useState } from 'react'  
 import { Link } from 'react-router-dom'
+             
+
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+    })
+
+    const handleChange = e =>{
+        setInputs(prev=>({...prev,[e.target.name]: e.target.value}))
+
+    }
+
+    console.log(inputs)
+
     return (
         <div className='auth'>
             <h1>Registrate</h1>
             <form>
-                <input required type="text" placeholder='username'/>
-                <input required type="email" placeholder='email'/>
-                <input required type="password" placeholder='password'/>
+                <input required type="text" placeholder='username' name='username' onChange={handleChange}/>
+                <input required type="email" placeholder='email' name='email' onChange={handleChange}/>
+                <input required type="password" placeholder='password' name='password' onChange={handleChange}/>
                 <button>Registrate</button>
                 <p>Esto es un error!</p>
                 <span>
-                Ya tienes una cuenta? <a href="/Login">Inicia</a>           
+                Ya tienes una cuenta? <Link to="/Login">Inicia</Link>           
                 </span>
                 
             </form>
